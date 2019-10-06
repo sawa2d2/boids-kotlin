9f1fdd4e1b48bbfd682b9b2eda22c5fb92fe2a02
sawa2d2 <sawa.2d2@hotmail.com>
2019-10-06 19:55:52 +0900

    minor change

--- step1-setup/src/main/kotlin/boids/Boids.kt
+++ step1-setup/src/main/kotlin/boids/Boids.kt
@@ -3,18 +3,23 @@ package boids
 import org.nd4j.linalg.api.ndarray.INDArray
 import org.nd4j.linalg.factory.Nd4j
 
-class Boids() {
-    var p = Nd4j.create(doubleArrayOf(0.0, 100.0))
-    var v = Nd4j.create(doubleArrayOf(1.0, 0.0))
-    
-    fun run(){
+class Boid(var p: INDArray, var v: INDArray) {
+    fun act() {
         p = p.add(v)
         if(p.getDouble(0) >= 200) {
             p.putScalar(0, 0)
-        } 
+        }
+    }
+}
+
+class Boids() {
+    var boid = Boid(Nd4j.create(doubleArrayOf(0.0, 100.0)), Nd4j.create(doubleArrayOf(1.0, 0.0)))
+
+    fun run() {
+        boid.act()
     }
 
     fun getData(): FloatArray {
-        return floatArrayOf(p.getFloat(0), p.getFloat(1))
+        return floatArrayOf(boid.p.getFloat(0), boid.p.getFloat(1))
     }
 }
