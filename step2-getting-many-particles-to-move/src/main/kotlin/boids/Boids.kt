1a162b403d9ea08a9bb94443d82929db05579adb
sawa2d2 <sawa.2d2@hotmail.com>
2019-10-06 20:57:46 +0900

    step2: implement rebounding

--- step2-getting-many-particles-to-move/src/main/kotlin/boids/Boids.kt
+++ step2-getting-many-particles-to-move/src/main/kotlin/boids/Boids.kt
@@ -3,15 +3,37 @@ package boids
 import org.nd4j.linalg.api.ndarray.INDArray
 import org.nd4j.linalg.factory.Nd4j
 
-class Boid(var p: INDArray, var v: INDArray) {
+class Boid(var p: INDArray, var v: INDArray, val rangeMin: INDArray, val rangeMax: INDArray) {
     fun act() {
         p = p.add(v)
+        fix()
+    }
+
+    fun fix() {
+        for(d in 0..1) {
+            if(p.getDouble(d) <= rangeMin.getDouble(d)) {
+               p.putScalar(d, rangeMin.getDouble(d))
+               v.putScalar(d, -v.getDouble(d))
+            }
+            if(p.getDouble(d) >= rangeMax.getDouble(d)) {
+               p.putScalar(d, rangeMax.getDouble(d))
+               v.putScalar(d, -v.getDouble(d))
+            }
+        }
     }
 }
 
 class Boids() {
+    val rangeMin = Nd4j.create(doubleArrayOf(0.0, 0.0))
+    val rangeMax = Nd4j.create(doubleArrayOf(600.0, 600.0))
+
     val boids = Array(100, {
-        Boid(Nd4j.rand(2, 1).mul(600.0), Nd4j.rand(2, 1).sub(0.5).mul(5 * 2.0))
+        Boid(
+            p = Nd4j.rand(2, 1).mul(600.0),
+            v = Nd4j.rand(2, 1).sub(0.5).mul(5 * 2.0),
+            rangeMin = rangeMin,
+            rangeMax = rangeMax
+        )
     })
 
     fun run() {
